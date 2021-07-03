6cb12891908a3ff78e649263bf1b1760177152f7
nedvelkov <nedyalko.velkov@gmail.com>
2021-07-03 13:55:01 +0300

    Add initial create
    Add create Db models, Db context and migrations for Db.
    Add part of view models and controller for Admin user type, add razor view pages for created view models.

---
+++ DarkBattle/Data/Models/Merchant.cs
@@ -0,0 +1,22 @@
+﻿namespace DarkBattle.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Merchant
+    {
+        [Key]
+        [Required]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+        public ICollection<Consumable> Consumables { get; set; } = new List<Consumable>();
+    }
+}
---
+++ DarkBattle/DataConstants/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkBattle.DataConstants
+{
+    public class Constants
+    {
+        //Common constants
+        public const string ImageRegex = @"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)";
+        public const int NameMinLenght = 3;
+        public const int NameMaxLenght = 15;
+        public const int MinValue = 1;
+        public const int MaxGoldDrop=250;
+        public const int DescriptionMaxLenght = 250;
+
+        //Creature
+        public const int MaxLevelCreature = 50;
+
+    }
+}
