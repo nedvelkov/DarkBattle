962a293c1d9f57e6de19db56d32cb917514a4f4c
nedvelkov <nedyalko.velkov@gmail.com>
2021-07-04 17:38:30 +0300

    edit connection string at ConfigureServices

--- DarkBattle/Startup.cs
+++ DarkBattle/Startup.cs
@@ -27,8 +27,9 @@ namespace DarkBattle
         {
             services.AddControllersWithViews();
             //TODO : Configurate Db
-            services.AddDbContext<DarkBattleDbContext>
-                (opt => opt.UseSqlServer(@"Server=.;Database=QuizSystem;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            services.AddDbContext<DarkBattleDbContext>(opt => 
+                    opt.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,8 @@ namespace DarkBattle
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
+
             }
             else
             {
