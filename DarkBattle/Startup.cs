f0449a3d9f539c86dc5c5a99e7adb67c20d19e91
nedvelkov <nedyalko.velkov@gmail.com>
2021-07-09 19:23:26 +0300

    add image to Champion class list view

--- DarkBattle/Startup.cs
+++ DarkBattle/Startup.cs
@@ -1,28 +1,21 @@
-using DarkBattle.Data;
-using DarkBattle.MappingConfiguration;
-using DarkBattle.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace DarkBattle
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    using DarkBattle.Data;
+    using DarkBattle.MappingConfiguration;
+    using DarkBattle.Services;
+    using DarkBattle.Infrastructure;
+
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
@@ -38,24 +31,26 @@ namespace DarkBattle
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
 
-            services.AddAutoMapper(typeof(DarkBattleProfile));
-            services.AddTransient<ICreatureService, CreatureService>();
-            services.AddTransient<IAreaService, AreaService>();
-            services.AddTransient<IValidator, Validator>();
-            services.AddTransient<IItemService, ItemService>();
-            services.AddTransient<IConsumableService, ConsumableService>();
-            services.AddTransient<IMerchantService, MerchantService>();
-            services.AddTransient<IChampionClassService, ChampionClassService>();
+            services.AddAutoMapper(typeof(DarkBattleProfile))
+                    .AddTransient<ICreatureService, CreatureService>()
+                    .AddTransient<IAreaService, AreaService>()
+                    .AddTransient<IValidator, Validator>()
+                    .AddTransient<IItemService, ItemService>()
+                    .AddTransient<IConsumableService, ConsumableService>()
+                    .AddTransient<IMerchantService, MerchantService>()
+                    .AddTransient<IChampionClassService, ChampionClassService>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
+                app.UseDeveloperExceptionPage()
+                   .UseMigrationsEndPoint();
             }
             else
             {
@@ -63,21 +58,19 @@ namespace DarkBattle
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
+            app.UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthorization()
+                .UseAuthentication()
+                .UseEndpoints
+                    (endpoints =>
+                        {
+                            endpoints.MapDefaultControllerRoute();
+                            endpoints.MapRazorPages();
+                        }
+                    );
         }
     }
 }
